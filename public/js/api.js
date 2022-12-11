40cdae9a0eec623e64a9a9e414b3e1ddd3147f23
Nadilson <nadilson@protonmail.com>
2022-12-11 04:22:58 -0300

    Acesso à API, smooth scroll/scrollbar fixes, suporte a CORS

---
+++ public/js/api.js
@@ -0,0 +1,11 @@
+// É necessário criar uma conta em https://www.themoviedb.org
+// e, uma vez logado, criar as configurações de acesso à API
+// do app.
+
+let api_key = "YOUR_API_KEY_HERE";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const port = 3333;
 
 let initialPath = path.join(__dirname, "public");
 
 let app = express();
 app.use(express.static(initialPath));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(initialPath, "index.html"));
